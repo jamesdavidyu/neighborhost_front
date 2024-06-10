1c8a484eea037c0d17f497e38b74a477283118ee
You Young June <youngjune@planorban.com>
2024-06-09 21:34:12 -0400

    added refresh token functionality to keep users logged in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/landing/Landing';
 import Login from './pages/login/Login';
+import { BaseUrl } from './BaseUrl';
 
 export const LoginContext = createContext()
 
 function App() {
+  useEffect(() => {
+    function refreshTokens() {
+      if(localStorage.refresh) {
+        const url = BaseUrl + 'api/token/refresh';
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type' : 'application/json'
+          },
+          body: JSON.stringify({
+            refresh: localStorage.refresh,
+          }),
+        })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          localStorage.acceess = data.access;
+          localStorage.refresh = data.refresh;
+          setLoggedIn(true);
+        });
+      }
+    }
+    const minute = 1000 * 60
+    refreshTokens();
+    setInterval(refreshTokens, minute * 3);
+  }, []);
+
   const [loggedIn, setLoggedIn] = useState(
     localStorage.access ? true : false
   );
