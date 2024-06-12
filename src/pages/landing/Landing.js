0ae3a5244a407ff78889cdace5cc689bfe11c7d3
You Young June <youngjune@planorban.com>
2024-06-11 21:46:14 -0400

    users can sign up and if user logged in and tries to go to landing page, redirects to login page

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import '../../index.css';
 import './in_landing/Landing.css';
 import LoginForm from './in_landing/LoginForm';
@@ -6,8 +7,41 @@ import CreateAccountButton from './in_landing/CreateAccountButton';
 import ForgotPassword from './in_landing/ForgotPassword';
 import LandingEvents from './in_landing/LandingEvents';
 import HomeButton from '../../HomeButton';
+import { BaseUrl } from '../../BaseUrl';
+import { LoginContext } from '../../App';
 
 function Landing() {
+    const [loggedIn, setLoggedIn] = useContext(LoginContext);
+    const [landing, setLanding] = useState();
+
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    useEffect(() => {
+        const url = BaseUrl + 'api/login/'; // need API to respond with event data also
+        fetch(url, {
+            headers: {
+                'Content-Type' : 'application/json',
+                Authorization : 'Bearer ' + localStorage.getItem('access')
+            },
+        })
+        .then((response) => {
+            if(response.status === 200){
+                navigate('/login')
+            } else if(response.status === 401){
+                setLoggedIn(false);
+                navigate('/', {
+                    state: {
+                        previousUrl: location.pathname,
+                    }
+                }); // can I do a separate fetch in here to get landing data?
+            }
+            return response.json();
+        }).then((data) => {
+            setLanding(data.landing);
+        });
+    }, []);
+
     return (
         <div className="quarter_container">
             <div className="quarter_columnize left_column">
@@ -18,6 +52,9 @@ function Landing() {
             </div>
             <div className="quarter_columnize right_column">
                 <LandingEvents />
+                {landing ? landing.map((land) => {
+                    return <p>{land.neighborhood}</p>;
+                }) : null}
             </div>
         </div>
     );
--- src/pages/landing/in_landing/CreateAccountForm.js
+++ src/pages/landing/in_landing/CreateAccountForm.js
@@ -1,31 +1,110 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useNavigate} from 'react-router-dom';
 import '../../../index.css';
 import './Landing.css';
+import { BaseUrl } from '../../../BaseUrl';
+import { LoginContext } from '../../../App';
 
 function CreateAccountForm() {
+    const [loggedIn, setLoggedIn] = useContext(LoginContext);
+    const [email, setEmail] = useState();
+    const [username, setUsername] = useState();
+    const [zipcode, setZipcode] = useState();
+    const [password, setPassword] = useState();
+
+    const navigate = useNavigate();
+
+    function Signup(e) {
+        e.preventDefault();
+        const url = BaseUrl + 'api/signup/';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({
+                email: email,
+                username: username,
+                zipcode: zipcode, 
+                password: password
+            }),
+        })
+        .then((response) => {return response.json();})
+        .then((data) => {
+            localStorage.setItem('access', data.access);
+            localStorage.setItem('refresh', data.refresh);
+            setLoggedIn(true);
+            navigate('/verify_account');
+        });
+    }
     return (
         <div>
             <h1>Welcome to the neighborhost!</h1>
             <h4 style={{ fontWeight: 'normal' }}>We need the following information to set up your account:</h4>
-            <form>
+            <form id="signup" onSubmit={Signup}>
                 <div>
-                    <input type="email" placeholder="Email" className="format" style={{ width: "90.25%", height: "29.5px", fontSize: "15.92px" }} />
+                    <input 
+                        type="email"
+                        id="email"
+                        placeholder="Email" 
+                        className="format" 
+                        style={{ width: "90.25%", height: "29.5px", fontSize: "15.92px" }}
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                    />
                 </div>
                 <br />
                 <div>
-                    <input type="text" placeholder="Username" className="format" style={{ width: "90.25%", height: "29.5px", fontSize: "15.92px" }} />
+                    <input 
+                        type="text"
+                        id="username"
+                        placeholder="Username" 
+                        className="format" 
+                        style={{ width: "90.25%", height: "29.5px", fontSize: "15.92px" }}
+                        value={username}
+                        onChange={(e) => {
+                            setUsername(e.target.value);
+                        }}
+                    />
                 </div>
                 <br />
                 <div>
-                    <input type="text" placeholder="Zipcode" className="format" style={{ height: "29.5px", fontSize: "15.92px" }} />
+                    <input 
+                        type="text"
+                        id="zipcode"
+                        placeholder="Zipcode" 
+                        className="format" 
+                        style={{ height: "29.5px", fontSize: "15.92px" }}
+                        value={zipcode}
+                        onChange={(e) => {
+                            setZipcode(e.target.value);
+                        }}
+                    />
                 </div>
                 <br />
                 <div>
-                    <input type="password" placeholder="Password" className="format" style={{ width: "90.25%", height: "29.5px", fontSize: "15.92px" }} />
+                    <input 
+                        type="password" 
+                        placeholder="Password" 
+                        className="format" 
+                        style={{ width: "90.25%", height: "29.5px", fontSize: "15.92px" }} 
+                    />
                 </div>
                 <br />
                 <div>
-                    <input type="password" placeholder="Confirm password" className="format" style={{ width: "90.25%", height: "29.5px", fontSize: "15.92px" }} />
+                    <input 
+                        type="password"
+                        id="password"
+                        placeholder="Confirm password" 
+                        className="format" 
+                        style={{ width: "90.25%", height: "29.5px", fontSize: "15.92px" }}
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                    />
                 </div>
                 <br />
                 <div>
